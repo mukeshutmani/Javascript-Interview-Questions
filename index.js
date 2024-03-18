171bb71f897e1167ce2621ab3ca0eb90af3acdec
mukesh <kumarmukesh789123@gmail.com>
2024-03-18 11:28:13 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+// function add(a,b) {
+//     return a+b;
+// }
+// const result=add(2,6)
+// console.log(result)
+// const f =(x,y)=> x+y;
+// console.log(f(3,4))
+// console.log([] == ![])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
